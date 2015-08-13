d6829ddda7f6c20ae8e25be9f3db93c068f7e78a
Christopher Atlan <catlan@creativeinaustria.com>
2015-08-13 15:12:23 +0200

    Initial Commit

---
+++ LOZIPFileWrapper/LOZIPFileWrapper.h
@@ -0,0 +1,19 @@
+//
+//  LOZIPFileWrapper.h
+//  LOZIPFileWrapper
+//
+//  Created by Christopher Atlan on 13/08/15.
+//  Copyright (c) 2015 Christopher Atlan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LOZIPFileWrapper.
+FOUNDATION_EXPORT double LOZIPFileWrapperVersionNumber;
+
+//! Project version string for LOZIPFileWrapper.
+FOUNDATION_EXPORT const unsigned char LOZIPFileWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LOZIPFileWrapper/PublicHeader.h>
+
+
