catlan/LOZIPFileWrapper

commit 452a0eaf6b879da63bc27525dd1f42e95ed613ac

Change includeFolders behaviour
Some zip apps include the folder some don't. In case of includeFolders we now overwrite folder attributes to make sure that every folder has NSFileType NSFileTypeDirectory.

LOZIPFileWrapper/LOZIPFileWrapper.m
@@ -342,13 +342,6 @@ NSString *const LOZIPFileWrapperMinizipErrorCode = @"LOZIPFileWrapperErrorDomain
         
         if (contentsOfArchive)
         {
-            NSString *parentPath = [filename stringByDeletingLastPathComponent];
-            while (includeFolders && ![parentPath isEqualToString:@""] &&  ![contentsOfArchive valueForKey:parentPath])
-            {
-                // this makes a wrong file list order because /1/2/3 would show up before /1/2.
-                contentsOfArchive[parentPath] = @{ NSFileType : NSFileTypeDirectory };
-                parentPath = [parentPath stringByDeletingLastPathComponent];
-            }
             contentsOfArchive[filename] = itemAttributes;
         }
         
@@ -360,6 +353,22 @@ NSString *const LOZIPFileWrapperMinizipErrorCode = @"LOZIPFileWrapperErrorDomain
     {
         NSERROR_GO_TO_NEXT_FILE(error, err);
         return nil;
+    }
+    
+    if (includeFolders)
+    {
+        NSMutableDictionary *contentsOfArchiveIncludingFolders = [contentsOfArchive mutableCopy];
+        for (NSString *path in contentsOfArchive)
+        {
+            NSString *basepath = [path stringByDeletingLastPathComponent];
+            if ([basepath isEqual:@""])
+            {
+                continue;
+            }
+            
+            contentsOfArchiveIncludingFolders[basepath] = @{ NSFileType : NSFileTypeDirectory };
+        }
+        contentsOfArchive = contentsOfArchiveIncludingFolders;
     }
     
     return [NSDictionary dictionaryWithDictionary:contentsOfArchive];

commit fec21201705c3e47f7312f2242363c6333f32230

That bug again
this time introduced in 452a0eaf6b879da63bc27525dd1f42e95ed613ac

LOZIPFileWrapper/LOZIPFileWrapper.m
@@ -360,13 +360,23 @@ NSString *const LOZIPFileWrapperMinizipErrorCode = @"LOZIPFileWrapperErrorDomain
         NSMutableDictionary *contentsOfArchiveIncludingFolders = [contentsOfArchive mutableCopy];
         for (NSString *path in contentsOfArchive)
         {
-            NSString *basepath = [path stringByDeletingLastPathComponent];
-            if ([basepath isEqual:@""])
+            NSString *basePath = [path stringByDeletingLastPathComponent];
+            while (![basePath isEqual:@""])
             {
-                continue;
+                NSDictionary *base = contentsOfArchiveIncludingFolders[basePath];
+                if (base)
+                {
+                    // In case the zip archive added a empty file as directory
+                    if (![base[NSFileType] isEqual:NSFileTypeDirectory])
+                    {
+                        contentsOfArchiveIncludingFolders[basePath] = @{ NSFileType : NSFileTypeDirectory };
+                    }
+                }
+                contentsOfArchiveIncludingFolders[basePath] = @{ NSFileType : NSFileTypeDirectory };
+                
+                basePath = [basePath stringByDeletingLastPathComponent];
             }
             
-            contentsOfArchiveIncludingFolders[basepath] = @{ NSFileType : NSFileTypeDirectory };
         }
         contentsOfArchive = contentsOfArchiveIncludingFolders;
     }